1625d1ad804042eca9e5950c4345ff326d942d78
unknown <duraisudhakarmurugesan@gmail.com>
2024-03-31 17:33:08 +0530

    delete method implemented

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,16 +3,27 @@ import { v4 as uuid4 } from "uuid";
 
 const router = express.Router();
 
-const array = [];
+let array = [];
+
+router.get('/', (req, res)=>{
+  res.send(JSON.stringify(array));
+})
 
 router.post('/', (req, res)=>{
-    const ob = {
-      id: uuid4(),
-      title: req.body.title,
-      description: req.body.description
-    };
-    array.push(ob);
-    res.send(array);
+  const ob = {
+    id: uuid4(),
+    title: req.body.title,
+    description: req.body.description
+  };
+  array.push(ob);
+  console.log(array.length);
+});
+
+router.delete('/:id', (req, res)=>{
+  const arr = array.filter((a) => a.id != req.params["id"]);
+  res.send(arr);
+  array = arr;
+  console.log(array.lenght)
 })
 
 export default router;
